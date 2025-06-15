e4d8b80459d5747e0d035d95197c3f794d7be4bb
Aniket Kumar <kkaniket23@gmail.com>
2025-06-15 13:42:18 +0530

    breadcrumb added

--- src/layout/partials/HeaderComp.js
+++ src/layout/partials/HeaderComp.js
@@ -3,27 +3,16 @@ import { Navbar, Nav } from 'react-bootstrap'
 import  logo  from "../../assets/img/logo.png";
 export const Header = () => {
   return (
-    // <Navbar collapseOnSelect  bg='info' variant="dark" expand="md">
-    //     <Navbar.Brand>
-    //         <img src ={logo} alt="logo" width= "50px" />
-    //     </Navbar.Brand>
-    //     <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-    //     <Navbar.Collapse id = "basic-navbar-nav">
-    //         <Nav>
-    //             <Nav.Link href = "/dashboard">Dashboard</Nav.Link>
-    //             <Nav.Link href = "/dashboard">Tickets</Nav.Link>
-    //             <Nav.Link href = "/dashboard">Logout</Nav.Link>
-    //         </Nav>
-    //     </Navbar.Collapse>
-    // </Navbar>
-    <Navbar collapseOnSelect bg ='info' variant='dark' expand='md'>
+    <Navbar collapseOnSelect  bg='info' variant="dark" expand="md">
         <Navbar.Brand>
-            <img src={logo} alt='logo' width='50px'/>
+            <img src ={logo} alt="logo" width= "50px" />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls='basic-navbar-nav'/>
-        <Navbar.Collapse id='basic-navbar-nav'>
-            {/* <Nav className='ml-auto'>
-                <Nav.Link href='/dashboard'>Dashboard</Nav.Link>
+        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+        <Navbar.Collapse id = "basic-navbar-nav">
+            {/* <Nav>
+                <Nav.Link href = "/dashboard">Dashboard</Nav.Link>
+                <Nav.Link href = "/dashboard">Tickets</Nav.Link>
+                <Nav.Link href = "/dashboard">Logout</Nav.Link>
             </Nav> */}
         </Navbar.Collapse>
     </Navbar>
