391660a13ae68e181411b4f1cce98d3bdf3fb89f
Aniket Kumar <kkaniket23@gmail.com>
2025-06-15 13:32:34 +0530

    added dashboard

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 // import { Entry } from './pages/entry/EntryPage';
 import { DefaultLayout } from './layout/DefaultLayout';
+import { Dashboard } from './pages/dashboard/DashboardPage';
 
 
 function App() {
   return (
     <div className="App">
       <DefaultLayout>
-        
+        <Dashboard />
       </DefaultLayout>
     </div>
   );
