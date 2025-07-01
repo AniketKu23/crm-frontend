da4b08f97d3137df0f10ebc0fec2f8089a6d9b80
Aniket Kumar <kkaniket23@gmail.com>
2025-07-01 19:56:00 +0530

    Added Redux toolkit to manage react states

--- src/components/search-form/SearchFormComp.js
+++ src/components/search-form/SearchFormComp.js
@@ -1,32 +1,33 @@
-import React from 'react'
-import { Form, Row, Col } from 'react-bootstrap'
-import PropTypes from 'prop-types'
+import React from "react";
+import { Form, Row, Col } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+import { filterSerachTicket } from "../../pages/ticket-list/TicketAction";
 
+export const SearchForm = () => {
+  const dispatch = useDispatch();
 
-export const SearchForm = ({handleOnChange,str}) => {
+  const handleOnChange = (e) => {
+    const { value } = e.target;
+
+    dispatch(filterSerachTicket(value));
+  };
+
   return (
     <div>
-        <Form>
-            <Form.Group as={Row}>
-                <Form.Label column sm="3">
-                    Search:{" "}
-                </Form.Label>
-                <Col ms="9">
-                    <Form.Control
-                    name='SearchStr'
-                    onChange={handleOnChange}
-                    value={str}
-                    placeholder='Search ...'
-                    />
-                </Col>
-            </Form.Group>
-        </Form>
+      <Form>
+        <Form.Group as={Row}>
+          <Form.Label column sm="3">
+            Search:
+          </Form.Label>
+          <Col sm="9">
+            <Form.Control
+              name="searchStr"
+              onChange={handleOnChange}
+              placeholder="Search ..."
+            />
+          </Col>
+        </Form.Group>
+      </Form>
     </div>
-  )
-}
-
-
-SearchForm.prototype = {
-    handleOnChange:PropTypes.func.isRequired,
-    str:PropTypes.string.isRequired
-}
+  );
+};
--- src/components/ticket-table/TicketTableComp.js
+++ src/components/ticket-table/TicketTableComp.js
@@ -1,38 +1,52 @@
-import React from 'react'
-import {Row, Table} from 'react-bootstrap'
-import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Row, Table } from "react-bootstrap";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
+export const TicketTable = () => {
+  const { searchTicketList, isLoading, error } = useSelector(
+    (state) => state.tickets
+  );
 
-export const TicketTable = ({tickets}) => {
-  return <Table striped bordered hover>
-    <thead>
+  if (isLoading) return <h3>Loading...</h3>;
 
+  if (error) return <h3>{error}</h3>;
+
+  return (
+    <Table striped bordered hover>
+      <thead>
         <tr>
-
-            <th>#</th>
-            <th>Subjects</th>
-            <th>Status</th>
-            <th>Open Date</th>
+          <th>#</th>
+          <th>Subjects</th>
+          <th>Status</th>
+          <th>Open Date</th>
         </tr>
-    </thead>
-    <tbody>
-        {tickets.length ? tickets.map(row=><tr>
-            <td>{row.id}</td>
-            <td>
-                
-            <Link to={`/ticket/${row.id}`}>{row.subject}</Link></td>
-            <td>{row.status}</td>
-            <td>{row.addedAt}</td>
-        </tr>):
-        <tr>
-            <td colSpan="4" className='text-center'>No ticket to show</td>
-        </tr>
-        }
-    </tbody>
-  </Table>
-}
+      </thead>
+      <tbody>
+        {searchTicketList.length ? (
+          searchTicketList.map((row) => (
+            <tr key={row._id}>
+              <td>{row._id}</td>
+              <td>
+                <Link to={`/ticket/${row._id}`}>{row.subject}</Link>
+              </td>
+              <td>{row.status}</td>
+              <td>{row.openAt}</td>
+            </tr>
+          ))
+        ) : (
+          <tr>
+            <td colSpan="4" className="text-center">
+              No ticket to show
+            </td>
+          </tr>
+        )}
+      </tbody>
+    </Table>
+  );
+};
 
 TicketTable.propTypes = {
-    tickets : PropTypes.array.isRequired,
-}
+  tickets: PropTypes.array.isRequired,
+};
