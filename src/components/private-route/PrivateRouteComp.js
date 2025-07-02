c400f22c253af43bbff5bf92b46a3de6d76c2b47
Aniket Kumar <kkaniket23@gmail.com>
2025-07-02 13:04:23 +0530

    User auth added sith react-router-dom, jwt,redux

--- src/components/private-route/PrivateRouteComp.js
+++ src/components/private-route/PrivateRouteComp.js
@@ -1,10 +1,33 @@
-import React from 'react'
-import { Route, Redirect } from 'react-router-dom'
-import { DefaultLayout } from '../../layout/DefaultLayout'
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Route, Redirect } from "react-router-dom";
+import { loginSuccess } from "../login/loginSlice";
+import { getUserProfile } from "../../pages/dashboard/userAction";
 
-const isAuth = true
+import { fetchNewAccessJWT } from "../../api/userApi";
+
+import { DefaultLayout } from "../../layout/DefaultLayout";
 
 export const PrivateRoute = ({ children, ...rest }) => {
+  const dispatch = useDispatch();
+  const { isAuth } = useSelector((state) => state.login);
+  const { user } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    const updateAccessJWT = async () => {
+      const result = await fetchNewAccessJWT();
+      result && dispatch(loginSuccess());
+    };
+
+    !user._id && dispatch(getUserProfile());
+
+    !sessionStorage.getItem("accessJWT") &&
+      localStorage.getItem("crmSite") &&
+      updateAccessJWT();
+
+    !isAuth && sessionStorage.getItem("accessJWT") && dispatch(loginSuccess());
+  }, [dispatch, isAuth, user._id]);
+
   return (
     <Route
       {...rest}
@@ -15,11 +38,11 @@ export const PrivateRoute = ({ children, ...rest }) => {
           <Redirect
             to={{
               pathname: "/",
-              state: { from: location }
+              state: { from: location },
             }}
           />
         )
       }
     />
-  )
-}
+  );
+};
